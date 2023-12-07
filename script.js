3da03a1db5a254db8cf1063c4408ff64f5bf4af5
Omelianov Nicolai <omelianov.n@gmail.com>
2023-12-07 20:04:39 +0200

    let start

---
+++ script.js
@@ -0,0 +1,72 @@
+let watchId;
+let trackingInterval;
+
+function startTracking() {
+  if ('geolocation' in navigator) {
+    const coordinatesElement = document.getElementById('coordinateValues');
+
+    // Опции для первоначального запроса watchPosition
+    const initialOptions = { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 };
+
+    watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        console.log('New Location:', latitude, longitude);
+
+        // Обновляем текст в поле координат
+        coordinatesElement.textContent = `${latitude}, ${longitude}`;
+
+        // Здесь вы можете отправить данные на сервер через WebSocket или другим способом
+      },
+      (error) => {
+        handleGeolocationError(error, 'Initial geolocation request');
+      },
+      initialOptions
+    );
+
+    // Устанавливаем интервал обновления геолокации каждые 3 секунды
+    trackingInterval = setInterval(() => {
+      // Опции для обновленного запроса getCurrentPosition
+      const updateOptions = { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 };
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          console.log('Updated Location:', latitude, longitude);
+
+          // Обновляем текст в поле координат
+          coordinatesElement.textContent = `${latitude}, ${longitude}`;
+
+          // Здесь также вы можете отправить данные на сервер
+        },
+        (error) => {
+          handleGeolocationError(error, 'Updated geolocation request');
+        },
+        updateOptions
+      );
+    }, 3000);
+  } else {
+    console.error('Geolocation is not supported');
+  }
+}
+
+function stopTracking() {
+  if (watchId) {
+    navigator.geolocation.clearWatch(watchId);
+    clearInterval(trackingInterval); // Очищаем интервал при остановке отслеживания
+    console.log('Tracking stopped');
+  }
+}
+
+function handleGeolocationError(error, context) {
+  if (error.code === 3) {
+    console.log(`${context} timed out`);
+  } else if (error.code === 1) {
+    console.log(`${context}: Location access denied by the user`);
+  } else {
+    console.error(`${context} error:`, error);
+  }
+}
+
+document.getElementById('startButton').addEventListener('click', startTracking);
+document.getElementById('stopButton').addEventListener('click', stopTracking);
