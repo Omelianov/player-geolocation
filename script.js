795791cda24fe9994635f90c524073fbc65ba3aa
Omelianov Nicolai <omelianov.n@gmail.com>
2023-12-08 19:13:23 +0200

    added name and user point

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let watchId;
 let trackingInterval;
 let map;
+let customMarker; // Добавим переменную для хранения маркера
 
+// Custom marker icon
+const customIcon = L.divIcon({
+  className: 'custom-marker'
+});
+
+// Initialize the map using Leaflet
 function initMap() {
   map = L.map('map').setView([0, 0], 14);
+
+  // Add OpenStreetMap layer
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+    attribution: '© OpenStreetMap contributors',
   }).addTo(map);
+
+  // Добавим маркер с начальными координатами
+  const initialCoords = [0, 0];
+  customMarker = L.marker(initialCoords, { icon: customIcon }).addTo(map);
 }
 
+// Function to start tracking
 function startTracking() {
   const startButton = document.getElementById('startButton');
   startButton.disabled = true;
@@ -16,19 +30,23 @@ function startTracking() {
   if ('geolocation' in navigator) {
     const coordinatesElement = document.getElementById('coordinateValues');
 
+    // Options for the initial watchPosition request
     const initialOptions = { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 };
 
+    // Start watching position
     watchId = navigator.geolocation.watchPosition(
       (position) => {
-        if (watchId) { // Добавим проверку перед выполнением запросов
-          const { latitude, longitude } = position.coords;
-          console.log('New Location:', latitude, longitude);
+        const { latitude, longitude } = position.coords;
+        console.log('New Location:', latitude, longitude);
 
-          coordinatesElement.textContent = `${latitude}, ${longitude}`;
+        // Update text in the coordinates field
+        coordinatesElement.textContent = `${latitude}, ${longitude}`;
 
-          const playerLocation = new L.LatLng(latitude, longitude);
-          map.panTo(playerLocation);
-        }
+        // Move the custom marker to the new location
+        customMarker.setLatLng([latitude, longitude]);
+
+        // Center and pan the map to the new location
+        map.setView([latitude, longitude]);
       },
       (error) => {
         handleGeolocationError(error, 'Initial geolocation request');
@@ -36,47 +54,51 @@ function startTracking() {
       initialOptions
     );
 
+    // Set an interval to update the geolocation every 3 seconds
     trackingInterval = setInterval(() => {
-      if (watchId) { // Добавим проверку перед выполнением запросов
-        const updateOptions = { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 };
+      // Options for the updated getCurrentPosition request
+      const updateOptions = { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 };
 
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            if (watchId) { // Добавим проверку перед выполнением запросов
-              const { latitude, longitude } = position.coords;
-              console.log('Updated Location:', latitude, longitude);
+      // Get the updated position
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          console.log('Updated Location:', latitude, longitude);
 
-              coordinatesElement.textContent = `${latitude}, ${longitude}`;
+          // Update text in the coordinates field
+          coordinatesElement.textContent = `${latitude}, ${longitude}`;
 
-              const playerLocation = new L.LatLng(latitude, longitude);
-              map.panTo(playerLocation);
-            }
-          },
-          (error) => {
-            handleGeolocationError(error, 'Updated geolocation request');
-          },
-          updateOptions
-        );
-      }
+          // Move the custom marker to the updated location
+          customMarker.setLatLng([latitude, longitude]);
+
+          // Center and pan the map to the updated location
+          map.setView([latitude, longitude]);
+        },
+        (error) => {
+          handleGeolocationError(error, 'Updated geolocation request');
+        },
+        updateOptions
+      );
     }, 3000);
   } else {
     console.error('Geolocation is not supported');
   }
 }
 
+// Function to stop tracking
 function stopTracking() {
   const startButton = document.getElementById('startButton');
   startButton.disabled = false;
 
   if (watchId) {
+    // Clear the geolocation watch and tracking interval
     navigator.geolocation.clearWatch(watchId);
     clearInterval(trackingInterval);
-    watchId = null; // Установим watchId в null, чтобы указать, что трекинг остановлен
     console.log('Tracking stopped');
   }
 }
 
-
+// Handle geolocation errors
 function handleGeolocationError(error, context) {
   if (error.code === 3) {
     console.log(`${context} timed out`);
@@ -87,5 +109,14 @@ function handleGeolocationError(error, context) {
   }
 }
 
+// Function to update marker text
+function updateMarkerText() {
+  const markerText = document.getElementById('markerText').value;
+  // Update the content of the custom marker with the entered text
+  customMarker.bindPopup(markerText).openPopup();
+}
+
+// Event listeners for the start, stop, and update buttons
 document.getElementById('startButton').addEventListener('click', startTracking);
 document.getElementById('stopButton').addEventListener('click', stopTracking);
+document.getElementById('updateButton').addEventListener('click', updateMarkerText);
